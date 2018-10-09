b77f9b974210c9afe95b53e622ecce6232a62668
КОТ <@DESKTOP-OIRAI1U>
2018-10-09 21:09:59 +0300

    Home Work

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -10,11 +10,61 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp2
 {
-    public partial class Form1 : Form
+    public partial class MyForm : Form
     {
-        public Form1()
+        bool enableClosing = false;
+        MyLock @lock = new MyLock("123456789");
+
+
+        public MyForm()
         {
+            @lock.Unlock += lock_Unlock;
+            @lock.LockAll += BlockAllButtons;
+
             InitializeComponent();
+        }
+        /// <summary>
+        /// Разблокирование сейфа
+        /// </summary>
+        private void lock_Unlock()
+        {
+            enableClosing = true;
+            Close();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+
+            @lock.Check(b.Text[0]);
+        }
+
+        /// <summary>
+        /// Закрытие формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (enableClosing) return;
+            e.Cancel = true;
+        }
+
+        /// <summary>
+        /// Блокировка всех кнопок
+        /// </summary>
+        private void BlockAllButtons()
+        {
+            foreach (Control control in this.Controls)
+            {
+                var but = control as Button;
+                
+                if (but == null) 
+                    continue;
+
+                but.Enabled = false;
+
+            }
         }
     }
 }
---
+++ WindowsFormsApp2/MyLock.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp2
+{
+    delegate void UnlockDelegate();
+    delegate void BlockButtonsDelegate();
+
+    internal class MyLock
+    {
+        char[] codeArr;
+        private int counter = 3; //счетчик попыток
+        public MyLock(string code)
+        {
+            codeArr = code.ToCharArray();
+        }
+        public event UnlockDelegate Unlock;
+        public event BlockButtonsDelegate LockAll;
+        List<char> inCode = new List<char>();
+
+        /// <summary>
+        /// Проверка правельности введенного символа
+        /// </summary>
+        /// <param name="code"></param>
+        public void Check(char code)
+        {
+            inCode.Add(code);
+            for (int i = 0; i < inCode.Count; i++)
+            {
+                if (codeArr[i] != inCode[i])
+                {
+                    inCode.Clear();
+                    counter--;
+                    if (counter == 0)
+                    {
+                        LockAll?.Invoke();
+                    }
+                    return;
+                }
+            }
+            if (inCode.Count == codeArr.Length)
+            {
+                Unlock?.Invoke();
+            }
+            
+
+        }
+    }
+}
